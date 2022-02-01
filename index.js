bc8ef4ace0d8e2ef72bd8dad26ddba5e9c53416b
Rido saputra <96877785+Ridosaputra181@users.noreply.github.com>
2022-02-01 18:28:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let countEL = document.getElementById("count-el");
+let saveEL = document.getElementById("save-el");
+let count = 0;
+
+console.log(saveEL);
+
+function increment() {
+  count += 1;
+  countEL.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEL.textContent += countStr;
+  countEL.textContent = 0;
+  count = 0;
+}
